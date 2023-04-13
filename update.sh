c3398ad15e4e0906c43bde913bce83c6e98fdd21
Brett T. Warden <brett.t.warden@intel.com>
2023-04-12 17:16:15 -0700

    Add update script

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e -o pipefail
+
+PKG=minetest_game
+
+errexit() {
+	local ret=1
+	echo "Error: $1"
+	if [[ -n "$2" ]]; then
+		ret=2
+	fi
+	exit $ret
+}
+
+git pull --ff-only
+
+# Update the Makefile
+make update-versions
+
+URL=$(grep '^URL' Makefile  | awk '{ print $3 }')
+VERSION=$(perl -pe "s|^.*/${PKG}-([\d\.]+)\.[^/]+\$|\$1|" <<< ${URL})
+CURRENT_URL=$(perl -pe 's/^(\s*URL\s*:\s*)\S+/\$1/' ${PKG}.spec)
+CURRENT_VERSION="$(rpmspec --srpm -q --qf="%{VERSION}" $PKG.spec)"
+
+if [[ u"${CURRENT_URL}" == u"${URL}" ]]; then
+	echo "Current version is still current: ${CURRENT_URL}"
+	exit
+fi
+
+perl -pi -e "s|${CURRENT_URL}|${URL}|g" ${PKG}.spec
+perl -pi -e "s|${CURRENT_VERSION}|${VERSION}|g" ${PKG}.spec
+
+make generateupstream || exit 3
+
+make bumpnogit
+git add $PKG.spec Makefile release upstream
+git commit -s -m "Update to ${VERSION}"
+make koji-nowait
