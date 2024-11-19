51fc76ba62ae2de33ffeae93713321e154fdec0a
Brett T. Warden <brett.t.warden@intel.com>
2024-11-19 15:14:23 -0800

    Make update.sh extract release notes into commit message
    Also add "--force" argument to make update.sh run even when there's no
    change.

--- update.sh
+++ update.sh
@@ -12,11 +12,16 @@ errexit() {
 	exit $ret
 }
 
-echo -n 'Update the local git repository: '
-git pull --ff-only
+if [[ "$1" != "--force" ]]; then
+	echo -n 'Update the local git repository: '
+	git pull --ff-only
+fi
+
+echo 'Fetch the upstream releaseinfo'
+release=$(curl -s https://content.luanti.org/api/packages/Minetest/minetest_game/releases/)
 
 echo -n 'Find the commit hash for the latest release: '
-hash=$(curl -s https://content.luanti.org/api/packages/Minetest/minetest_game/releases/ | jq -r '.[0].commit')
+hash=$(jq -r '.[0].commit' <<< ${release})
 echo ${hash}
 
 if [[ "" == "${hash}" ]]; then
@@ -32,7 +37,9 @@ CURRENT_VERSION="$(rpmspec --srpm -q --qf="%{VERSION}" $PKG.spec)"
 
 if [[ "${CURRENT_URL}" == "${URL}" ]] && [[ "${CURRENT_VERSION}" == "${VERSION}" ]]; then
    echo "Current version is still current: ${CURRENT_URL}"
-   exit 0
+   if [[ "$1" != "--force" ]]; then
+	   exit 0
+   fi
 fi
 
 echo Update the Makefile
@@ -40,8 +47,10 @@ sed -i -e "s#^URL *=.*#URL = ${URL}#" Makefile
 perl -pi -e "s|${CURRENT_URL}|${URL}|g" ${PKG}.spec
 perl -pi -e "s|${CURRENT_VERSION}|${VERSION}|g" ${PKG}.spec
 
-echo Check whether anything actually changed
-git diff --no-patch --exit-code Makefile ${PKG}.spec && errexit "No change to Makefile or specfile"
+if [[ "$1" != "--force" ]]; then
+	echo Check whether anything actually changed
+	git diff --no-patch --exit-code Makefile ${PKG}.spec && errexit "No change to Makefile or specfile"
+fi
 
 echo Update the tarball
 make generateupstream || exit 3
@@ -49,7 +58,15 @@ make generateupstream || exit 3
 echo Commit the changes
 make bumpnogit
 git add ${PKG}.spec Makefile release upstream
-git commit -s -m "Update to ${VERSION}"
+# Generate commit message, including release notes for all versions since
+# CURRENT_VERSION
+(	echo "Update to ${VERSION}";
+	echo;
+	i=0;
+	while [[ $(jq -r ".[$i].commit" <<< ${release} ) != ${CURRENT_VERSION} ]]; do
+		jq -r ".[$i].release_notes" <<< ${release} ;
+		((i++));
+	done) | git commit -F -
 
 echo Send the package to koji
 make koji-nowait
